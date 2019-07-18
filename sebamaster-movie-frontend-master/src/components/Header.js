629169aa7421f8928ca32631ca9fbab15208df04
Gilles Tanson <gillestanson@googlemail.com>
2019-07-18 13:13:16 +0200

    Worked on the Menu UI

--- sebamaster-movie-frontend-master/src/components/Header.js
+++ sebamaster-movie-frontend-master/src/components/Header.js
@@ -1,26 +1,45 @@
 "use strict";
 
 import React from 'react';
-import { Toolbar, Button } from 'react-md';
-import { withRouter } from 'react-router-dom'
+import { withRouter } from 'react-router-dom';
+import { Button, Icon, Image, Menu, Segment, Sidebar, Grid } from 'semantic-ui-react'
 
 import KebabMenu from './KebabMenu';
+import SideBarMenu from './Sidebar';
 
 
 class Header extends React.Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            activeItem: 'ow'
+        }
+
+        this.handleItemClick = this.handleItemClick.bind(this);
+    }
+
+    handleItemClick(name) {(e, {name}) =>
+        this.setState({ activeItem: name })
+        console.log(name)
     }
 
     render() {
+        const { activeItem }= this.state;
+
         return (
-            <Toolbar
-                colored
-                nav={<Button onClick={() => this.props.history.push('/')} icon>home</Button>}
-                title={this.props.title}
-                actions={<KebabMenu id="toolbar-colored-kebab-menu" />}>
-            </Toolbar>
+            <Menu tabular>
+                <Menu.Item name='ow' active={activeItem === 'ow'} onClick={this.handleItemClick} />
+                <Menu.Item name='csgo' active={activeItem === 'csgo'} onClick={this.handleItemClick} />
+                <Menu.Item name='lol' active={activeItem === 'lol'} onClick={this.handleItemClick} />
+            </Menu>
+            // <Toolbar
+            //     colored
+            //     nav={<Button onClick={() => this.props.history.push('/')} icon>menu</Button>}
+            //     title={this.props.title}
+            //     actions={<KebabMenu id="toolbar-colored-kebab-menu" />}>
+            // </Toolbar>
+
         );
     }
 };
